506ad1b9e9da36a1685dac97dd54ac1f8fe9cfb4
jdarangom2012 <jdarangom2012@gmail.com>
2024-11-16 13:10:11 -0500

    Confuguracion App

---
+++ pAppCars/pAppCars/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pAppCars project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pAppCars.settings')
+
+application = get_asgi_application()
